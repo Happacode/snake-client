8304ca29fa0b3d83f4c0c9855d788f88207edc1b
Harp Sandhu <happacode@protonmail.com>
2021-04-07 14:08:09 -0700

    added connection and name to client

--- client.js
+++ client.js
@@ -11,8 +11,20 @@ const connect = function() {
   conn.on('data', function (data) {
     console.log(data);
   });
+  
+  conn.on('connect', function () {
+    console.log("Successfully connected to game server");
+  });
+  
+  conn.on('connect', () => {
+    conn.write('Name: HAP');
+  });
 
   return conn;
 }
 
 module.exports = connect;
+
+
+// To accomplish this, inside the connect function, use the .on method on our conn object to register a "connect" handler (a callback function). 
+// In the callback, print a message for us (the player) to see - something like "Successfully connected to game server" will suffice.
